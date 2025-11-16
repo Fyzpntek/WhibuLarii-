c9135767d2c7fdbc34b197881c3fd06957fece16
Fyzpntek <96363664+Fyzpntek@users.noreply.github.com>
2025-11-16 16:41:52 +0700

    Implement episode handler to fetch images

--- api/anime/episode/[slug].js
+++ api/anime/episode/[slug].js
@@ -1,1 +1,22 @@
+import { SITE, fetchHTML, parseHTML } from '../_helpers.js';
 
+export default async function handler(req, res) {
+  try {
+    const { slug } = req.query;
+
+    const url = SITE + '/' + slug;
+    const html = await fetchHTML(url);
+    const $ = parseHTML(html);
+
+    const images = [];
+    $('.post-content img, .entry img').each((i, el) => {
+      const src = $(el).attr('src');
+      if (src) images.push(src);
+    });
+
+    res.status(200).json({ success: true, images });
+
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Episode failed', error: err.message });
+  }
+}
