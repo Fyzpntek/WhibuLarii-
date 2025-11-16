665de5d8119d6e0155815d64e7ec22d588cfbf97
Fyzpntek <96363664+Fyzpntek@users.noreply.github.com>
2025-11-16 16:59:47 +0700

    Refactor anime search handler for improved parsing

--- api/anime/search/[keyword].js
+++ api/anime/search/[keyword].js
@@ -1,25 +1,40 @@
-
-import { SITE, fetchHTML, parseHTML } from '../_helpers.js';
+import { SITE, fetchHTML, parseHTML } from './_helpers.js';
 
 export default async function handler(req, res) {
   try {
     const { keyword } = req.query;
-
-    const url = SITE + '/?s=' + keyword;
+    const url = `${SITE}/?s=${keyword}`;
+    
     const html = await fetchHTML(url);
     const $ = parseHTML(html);
 
     const results = [];
-    $('.post, .bs').each((i, el) => {
-      const title = $(el).find('h2 a, .tt a').first().text().trim();
-      const link = $(el).find('h2 a, .tt a').first().attr('href');
-      const image = $(el).find('img').first().attr('src');
-      results.push({ title, link, image });
+
+    $('.venz ul li').each((i, el) => {
+      const parent = $(el).find('.detpost');
+
+      const link = parent.find('.thumb a').attr('href');
+      const title = parent.find('.thumbz h2.jdlflm').text().trim();
+      const image = parent.find('.thumbz img').attr('src');
+      const episode = parent.find('.epz').text().trim();
+      const day = parent.find('.epztipe').text().trim();
+      const date = parent.find('.newnime').text().trim();
+
+      if (title && link) {
+        results.push({
+          title,
+          link,
+          image,
+          episode,
+          day,
+          date
+        });
+      }
     });
 
     res.status(200).json({ success: true, data: results });
 
   } catch (err) {
-    res.status(500).json({ success: false, message: 'Search failed', error: err.message });
+    res.status(500).json({ success: false, message: err.message });
   }
 }
