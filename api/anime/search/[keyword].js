808cacbb6c5068b567d70746f2083ab4dea26550
Fyzpntek <96363664+Fyzpntek@users.noreply.github.com>
2025-11-16 17:43:59 +0700

    Refactor anime search handler to use new API

--- api/anime/search/[keyword].js
+++ api/anime/search/[keyword].js
@@ -1,40 +1,10 @@
-import { SITE, fetchHTML, parseHTML } from './_helpers.js';
-
 export default async function handler(req, res) {
+  const { keyword } = req.query;
   try {
-    const { keyword } = req.query;
-    const url = `${SITE}/?s=${keyword}`;
-    
-    const html = await fetchHTML(url);
-    const $ = parseHTML(html);
-
-    const results = [];
-
-    $('.venz ul li').each((i, el) => {
-      const parent = $(el).find('.detpost');
-
-      const link = parent.find('.thumb a').attr('href');
-      const title = parent.find('.thumbz h2.jdlflm').text().trim();
-      const image = parent.find('.thumbz img').attr('src');
-      const episode = parent.find('.epz').text().trim();
-      const day = parent.find('.epztipe').text().trim();
-      const date = parent.find('.newnime').text().trim();
-
-      if (title && link) {
-        results.push({
-          title,
-          link,
-          image,
-          episode,
-          day,
-          date
-        });
-      }
-    });
-
-    res.status(200).json({ success: true, data: results });
-
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
+    const r = await fetch(`https://sankavollerei.com/anime/search/${keyword}`);
+    const data = await r.json();
+    res.status(200).json({ success: true, data });
+  } catch (e) {
+    res.status(500).json({ success: false, error: e.toString() });
   }
 }
